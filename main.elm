ef8a0c8f11f04db1522f343934ef20ff24658024
Alexander Groß <agross@therightstuff.de>
2017-03-19 14:51:14 +0100

    format time

--- main.elm
+++ main.elm
@@ -2,20 +2,21 @@ module Clock exposing (..)
 
 import Html
 import Time exposing (Time)
+import Time.DateTime as DT exposing (DateTime)
 
 
 type alias Model =
-    Time
+    DateTime
 
 
 type Message
     = Tick Time
 
 
-main : Program Never Time Message
+main : Program Never Model Message
 main =
     Html.program
-        { init = ( 0, Cmd.none )
+        { init = ( DT.dateTime DT.zero, Cmd.none )
         , update = update
         , view = view
         , subscriptions = always everySecond
@@ -26,12 +27,17 @@ update : Message -> Model -> ( Model, Cmd Message )
 update msg model =
     case msg of
         Tick time ->
-            ( time, Cmd.none )
+            ( DT.fromTimestamp time, Cmd.none )
 
 
 view : Model -> Html.Html Message
 view time =
-    Html.text (toString time)
+    Html.text (timeString time)
+
+
+timeString : DateTime -> String
+timeString time =
+    time |> DT.toISO8601
 
 
 everySecond : Sub Message
