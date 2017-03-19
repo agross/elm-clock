e2e826a0a5428390429d8bdcc0adfd68d82a512c
Alexander Groß <agross@therightstuff.de>
2017-03-19 14:18:15 +0100

    baseline

---
+++ main.elm
@@ -0,0 +1,32 @@
+module Clock exposing (..)
+
+import Html
+import Time exposing (Time)
+
+
+type alias Model =
+    Time
+
+
+type Message
+    = Tick Time
+
+
+main : Program Never Time Message
+main =
+    Html.program
+        { init = ( 0, Cmd.none )
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
+
+
+update : Message -> Model -> ( Model, Cmd Message )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html.Html Message
+view time =
+    Html.text (toString time)
